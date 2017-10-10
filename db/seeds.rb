536281c9adbf5d6552daa7f2b554ffe66d672cb6
trevor malard <redsky.dz@gmail.com>
2017-10-10 14:58:58 +0900

    seeder properly works

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 5.times do |_|
-  tmp = Task.create
+  tmp = Task.create(name:"")
   tmp.name+="Task#{tmp.id}"
   tmp.save
 end
